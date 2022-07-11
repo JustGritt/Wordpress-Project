e26324a89e4ad86bac831c84acc61b08fcc02d7c
JustGritt <alexistan9712@gmail.com>
2022-07-11 10:56:45 +0200

    ✨ Add partials Partners & Services

--- wp-content/themes/ESGI_Custom/page.php
+++ wp-content/themes/ESGI_Custom/page.php
@@ -15,11 +15,16 @@ get_header();
 while ( have_posts() ) :
 	the_post();
 	get_template_part( 'template-parts/content/content-page' );
-
-	// If comments are open or there is at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) {
-		comments_template();
-	}
 endwhile; // End of the loop.
 
+show_post('our-services');  
+show_post('our-partners');  
+
 get_footer();
+
+
+function show_post($path) {
+	$post = get_page_by_path($path);
+	$content = apply_filters('the_content', $post->post_content);
+	echo $content;
+}
